567241f509b842e404d529272a15b6cab13e44fe
Amal Benfateh <amal.benfateh@gmail.com>
2026-07-27 20:26:37 +0100

    Improve docstring for bubble_sort function
    Corrected grammar and added explanation about the algorithm's name.

--- algorithms/Sorting algorithms/bubble_sort.py
+++ algorithms/Sorting algorithms/bubble_sort.py
@@ -1,6 +1,10 @@
 '''Bubble Sort is an algorithm which is used to sort a list of elements.
 it comparess two adjacent elements and then swaps them if they are not in order
-the process is repeated until no more swapping is needed'''
+the process is repeated until no more swapping is needed.
+the algorithm is called Bubble Sort , because with each iteration the smallest 
+element in the list bubbles up to the top, just like a water bubble rises up to 
+the water surface'''
+
 def bubble_sort(nlist):
     for i in range(len(nlist)):
         for j in range(len(nlist)-1,0,-1):
